c154b19fe61d8f5ff6e82fbeefe9d5c1508d7242
mujtabasaqib <mujtabasaqib99@gmail.com>
2024-11-10 02:42:09 +0500

    get tracker url, treating js file as module

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict';
-const fs = require('fs');
-const torrent = fs.readFileSync('puppy.torrent');
-console.log(torrent.toString('utf8'));
+import fs from 'fs';
+import bencode from 'bencode';
+
+function decodeData(uint8Array) {
+  return new TextDecoder('utf-8').decode(uint8Array);
+}
+
+//console.log(torrent.announce.toString('utf8'));
+
+const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
+
+const decodedAnnounce = decodeData(torrent.announce);
+console.log(decodedAnnounce);  // decoded URL
+
