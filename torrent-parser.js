b15d8eadc7a859bfb0ab1a32493811b95484c26f
mujtabasaqib <mujtabasaqib99@gmail.com>
2024-11-14 04:17:24 +0500

    added infohash and size functions to torrent-parser file

--- torrent-parser.js
+++ torrent-parser.js
@@ -1,15 +1,24 @@
 'use strict';
 import fs from 'fs';
 import bencode from 'bencode';
+import crypto from 'crypto';
 
 export function open(filePath){
   return bencode.decode(fs.readFileSync(filePath));
 }
 
 export function size(torrent){
+  const size = torrent.info.files ?
+    torrent.info.files.map(file => BigInt(file.length)).reduce((a, b) => a + b) :
+    BigInt(torrent.info.length);
 
+  // Convert BigInt to Buffer
+  const buffer = Buffer.alloc(8);
+  buffer.writeBigUInt64BE(size); 
+  return buffer;
 }
 
 export function infoHash(torrent){
-
+  const info = bencode.encode(torrent.info);
+  return crypto.createHash('sha1').update(info).digest();
 }
